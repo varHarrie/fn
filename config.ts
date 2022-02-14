51eb0ad37c8c3ffd8df14ae49eb5fd40ad435962
varHarrie <varharrie@gmail.com>
2022-02-14 20:17:16 +0800

    feat: improve container

--- config.ts
+++ config.ts
@@ -2,6 +2,7 @@ const config = {
   host: Deno.env.get("HOST") ?? "localhost",
   port: Deno.env.get("PORT") ?? "3000",
   functionDir: Deno.env.get("FUNCTION_DIR") ?? "./functions",
+  functionTimeout: parseInt(Deno.env.get("FUNCTION_TIMEOUT") ?? "1500"),
 };
 
 export default config;
--- routers/public-router.ts
+++ routers/public-router.ts
@@ -1,4 +1,5 @@
-import { Router, Request } from "https://deno.land/x/oak@v10.2.0/mod.ts";
+import { Context, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
+import { getQuery } from "https://deno.land/x/oak@v10.2.0/helpers.ts";
 import { Status } from "https://deno.land/std@0.123.0/http/http_status.ts";
 import { resolvePath } from "../utils/path.ts";
 import Container from "../utils/container/index.ts";
@@ -6,10 +7,11 @@ import config from "../config.ts";
 
 const publicRouter = new Router();
 
-async function createContext(request: Request) {
-  const headers = Object.fromEntries(request.headers);
+async function createContext(ctx: Context) {
+  const headers = Object.fromEntries(ctx.request.headers);
+  const query = getQuery(ctx);
 
-  const bodyData = await request.body();
+  const bodyData = await ctx.request.body();
   const body =
     bodyData.type === "json" || bodyData.type === "text"
       ? bodyData.value
@@ -17,10 +19,11 @@ async function createContext(request: Request) {
 
   return {
     body,
+    query,
     headers,
-    ip: request.ip,
-    url: request.url.toString(),
-    method: request.method,
+    ip: ctx.request.ip,
+    url: ctx.request.url.pathname,
+    method: ctx.request.method,
   };
 }
 
@@ -30,7 +33,7 @@ publicRouter.all("/f/:url+", async (ctx) => {
 
   const filePath = resolvePath(config.functionDir, url, method + ".js");
   let code: string;
-  let result: Record<string, unknown>;
+  let result: string | number | boolean | Record<string, unknown>;
 
   try {
     code = await Deno.readTextFile(filePath);
@@ -42,13 +45,11 @@ publicRouter.all("/f/:url+", async (ctx) => {
   }
 
   const container = new Container();
-  const context = await createContext(ctx.request);
+  const context = await createContext(ctx);
+  const options = { timeout: config.functionTimeout };
 
   try {
-    result = (await container.execute(code, context)) as Record<
-      string,
-      unknown
-    >;
+    result = await container.execute(code, context, options);
   } catch (error) {
     return ctx.throw(
       Status.InternalServerError,
