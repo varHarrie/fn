769e1affec3a71bf037565014f001d4df893a9da
varHarrie <varharrie@gmail.com>
2022-02-18 01:13:22 +0800

    chore: move data

--- config.ts
+++ config.ts
@@ -4,8 +4,8 @@ const config = {
   passwordSalt: Deno.env.get("PASSWORD_SALT") ?? "",
   jwtExpires: parseInt(Deno.env.get("JWT_EXPIRES") ?? "86400"),
   jwtKeyFile: Deno.env.get("JWT_KEY_FILE") ?? "jwt.key",
-  usersFile: Deno.env.get("USERS_FILE") ?? "users.json",
-  functionDir: Deno.env.get("FUNCTION_DIR") ?? "functions",
+  usersFile: Deno.env.get("USERS_FILE") ?? "data/users.json",
+  functionDir: Deno.env.get("FUNCTION_DIR") ?? "data/functions",
   functionTimeout: parseInt(Deno.env.get("FUNCTION_TIMEOUT") ?? "1500"),
 };
 
